41a92b6b560592e738b93862b2481027159d9301
Philipp Homberger <Philipp.Homberger@mail.schwarz>
2024-03-27 16:02:56 +0100

    add basic stuff

---
+++ application.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Routes defined in the routes package
+	routes := r.Group("/api")
+	{
+		routes.GET("/", getHome)
+		routes.GET("/user/:name", getUser)
+		routes.GET("/albums", getAlbums)
+		routes.POST("/albums", postAlbum)
+	}
+
+	r.Run(":8080")
+}
+
+func getHome(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Welcome to the API!",
+	})
+}
+
+func getUser(c *gin.Context) {
+	name := c.Param("name")
+	c.JSON(200, gin.H{
+		"message": "Hello, " + name + "!",
+	})
+}
