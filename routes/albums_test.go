ba2c2899ceca1d393ddb7f124e1e0bf25ddac00a
philipphomberger <philipp.homberger@gmail.com>
2024-03-30 19:19:30 +0100

    add first api tests

--- routes/albums_test.go
+++ routes/albums_test.go
@@ -1,26 +1,29 @@
 package routes
 
 import (
+	"ginapi/models"
+	"github.com/mjarkk/mongomock"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestAlbumsRoute(t *testing.T) {
+	db := mongomock.NewDB()
+	collection := db.Collection("Albums")
+	err := collection.Insert(models.Album{
+		Title:  "Let it Be",
+		Artist: "The Beatles",
+		Price:  12.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 	router := SetupRouter()
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/albums", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	//assert.Equal(t, "pong", w.Body.String())
-}
-
-func TestAlbumRoute(t *testing.T) {
-	router := SetupRouter()
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/albums/12345678", nil)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
